dd1ac8b6b145be59eba01186c48773753dda9064
firaskahlout <firaskahlout@outlook.com>
2020-09-19 22:39:37 +0300

    add project files

---
+++ CI_Demo/CI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_DemoApp.swift
+//  CI_Demo
+//
+//  Created by Firas AlKahlout on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct CI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
